dbc3e2d3da1a5d3c73af3e95169558ea37161523
kalonjis <kalonjistephane@gmail.com>
2024-10-23 15:18:37 +0200

    add navbar header + footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path:'', loadChildren: ()=> import('./features/features.module')
+      .then(m => m.FeaturesModule),
+  }
 ];
 
 @NgModule({
