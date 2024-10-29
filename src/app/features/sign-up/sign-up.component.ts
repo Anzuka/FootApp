27d1752ec14cde6fc95d3021b90f3df7246a6285
David VK <david.vankeer@gmail.com>
2024-10-29 13:37:37 +0100

    update css on signup component

--- src/app/features/sign-up/sign-up.component.ts
+++ src/app/features/sign-up/sign-up.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Calendar } from 'primeng/calendar';
 
 @Component({
   selector: 'app-sign-up',
