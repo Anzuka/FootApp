a03c3dca64473c1469991fc5b635c20e96c0c1ab
Stephane Kalonji <kalonjistephane@gmail.com>
2024-11-05 14:29:13 +0100

    deleting component "request-new-password-confirmation"

--- src/app/features/user/user-routing.module.ts
+++ src/app/features/user/user-routing.module.ts
@@ -3,15 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {SignUpComponent} from './sign-up/sign-up.component';
 import {RequestNewPasswordComponent} from './request-new-password/request-new-password.component';
-import {RequestNewPasswordConfirmationComponent
-} from './request-new-password-confirmation/request-new-password-confirmation.component';
 import {ResetPasswordComponent} from './reset-password/reset-password.component';
 
 const routes: Routes = [
   { path: 'user/login', component: LoginComponent},
   { path: 'user/signup', component: SignUpComponent},
   { path: 'user/request-new-password', component: RequestNewPasswordComponent},
-  { path: 'user/request-new-password-confirmation', component: RequestNewPasswordConfirmationComponent},
   { path: 'user/reset-password', component: ResetPasswordComponent},
 
 ];
