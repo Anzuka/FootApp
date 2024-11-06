c305ffe8a51a08a91e2a7574e112530f9432ac69
David VK <david.vankeer@gmail.com>
2024-11-06 15:38:57 +0100

    protect /tournament/new with AuthGuard

--- src/app/features/tournament/tournament-routing.module.ts
+++ src/app/features/tournament/tournament-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { TournamentListComponent } from './tournament.list/tournament.list.component';
 import { TournamentInfoComponent } from './tournament-info/tournament-info.component';
 import { TournamentFormComponent } from './tournament.form/tournament.form.component';
+import { AuthGuard } from '../../core/guards/auth.guard';
 
 const routes: Routes = [
     { path:'list', component: TournamentListComponent},
-    { path: 'new', component: TournamentFormComponent},
-    { path: 'details/:id', component: TournamentInfoComponent},
+    { 
+        path: 'new', 
+        component: TournamentFormComponent, 
+        canActivate: [AuthGuard] // Protects the 'new' route
+    },
+        { path: 'details/:id', component: TournamentInfoComponent},
 ]
 
 @NgModule({
