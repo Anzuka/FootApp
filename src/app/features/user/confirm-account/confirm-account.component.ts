b90162275eca3f2f012faa4b0fb119a36a1129bc
kalonjis <kalonjistephane@gmail.com>
2024-11-08 12:29:44 +0100

    refactoring: use of the FeedbackBase

--- src/app/features/user/confirm-account/confirm-account.component.ts
+++ src/app/features/user/confirm-account/confirm-account.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 import { UserService } from '../tools/user.service'; // Service pour gérer les utilisateurs
 import { ApiResponse } from '../tools/models/api-response'; // Modèle de la réponse d'API
 import { HttpErrorResponse } from '@angular/common/http';
+import {FeedbackBase} from '../../../shared/feedback/tools/feedback.base';
 
 @Component({
   selector: 'app-confirm-account',
@@ -10,11 +11,7 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./confirm-account.component.scss']
 })
 export class ConfirmAccountComponent implements OnInit {
-  feedbackMessage: string = '';         // Message de retour à afficher
-  isFeedbackSuccess!: boolean ;    // Indique si le message est un succès ou une erreur
-  showFeedback: boolean = false;        // Contrôle l'affichage du FeedbackComponent
-  buttonText: string = '';
-  buttonAction: () => void = () => {};
+  feedbackConfirmAccount: FeedbackBase = new FeedbackBase();
 
   constructor(
     private _route: ActivatedRoute,
@@ -31,7 +28,7 @@ export class ConfirmAccountComponent implements OnInit {
       this._userService.confirmAccount(token).subscribe({
           next: (response: ApiResponse) => {
             console.log("response ok : ", response);
-            this._displaySucces(response.message, "Login")
+            this.feedbackConfirmAccount.displaySuccess(response.message, "Login")
             this._navToLogin();
           },
           //ERROR TOKEN EXPIRE
@@ -43,7 +40,7 @@ export class ConfirmAccountComponent implements OnInit {
       });
     } else {
       // Si aucun token n'est présent, afficher un message d'erreur
-      this._displayError("No token provided. Please check the confirmation link in the email we sent you", 'Home')
+      this.feedbackConfirmAccount.displayError("No token provided. Please check the confirmation link in the email we sent you", 'Home')
       this._navToHome();
     }
   }
@@ -52,30 +49,15 @@ export class ConfirmAccountComponent implements OnInit {
   // region Methods
 
   private _navToLogin(){
-    this.buttonAction = () =>{
+    this.feedbackConfirmAccount.buttonAction = () =>{
       this._router.navigate(['user/login']);
     }
   }
 
   private _navToHome(){
-    this.buttonAction = () =>{
+    this.feedbackConfirmAccount.buttonAction = () =>{
       this._router.navigate(['']);
     }
-  }
-
-  private _displayFeedback(isSucces: boolean, message: string, buttonText: string) {
-    this.showFeedback = true;
-    this.isFeedbackSuccess = isSucces;
-    this.feedbackMessage = message;
-    this.buttonText = buttonText;
-  }
-
-  private _displaySucces(message: string, buttonText: string) {
-    this._displayFeedback(true, message, buttonText);
-  }
-
-  private _displayError(message: string, buttonText: string) {
-    this._displayFeedback(false, message, buttonText);
   }
 
   private _handleErrors(error: HttpErrorResponse) {
@@ -88,33 +70,33 @@ export class ConfirmAccountComponent implements OnInit {
     console.error("confirmation error : ", error.error);
 
     if (errorType === 'expiration') {
-      this._displayError(errorMessage, 'Request a new Activation link')
+      this.feedbackConfirmAccount.displayError(errorMessage, 'Request a new Activation link')
       if (errorUrl) {
-        this.buttonAction = () => {
+        this.feedbackConfirmAccount.buttonAction = () => {
           this._userService.requestNewLink(errorUrl).subscribe({
             next: (response: ApiResponse) => {
               console.log("request token data : ",response);
-              this._displaySucces(response.message, "Home")
+              this.feedbackConfirmAccount.displaySuccess(response.message, "Home")
               this._navToHome();
             },
             error: (requestError: HttpErrorResponse) => {
               console.error("Error requesting new token: ", requestError);
-              this._displayError(requestError.error, "Home" )
+              this.feedbackConfirmAccount.displayError(requestError.error, "Home" )
               this._navToHome();
             }
           })
         };
       }
     } else if(errorType === 'activation'){
-        this._displayError(errorMessage, 'Login')
+        this.feedbackConfirmAccount.displayError(errorMessage, 'Login')
         this._navToLogin();
     }
     else if(errorType === 'validity'){
-      this._displayError(errorMessage, 'Home')
+      this.feedbackConfirmAccount.displayError(errorMessage, 'Home')
       this._navToHome();
     }
     else {
-      this._displayError("An error occurred while activating your account.", 'Home')
+      this.feedbackConfirmAccount.displayError("An error occurred while activating your account.", 'Home')
       this._navToHome();
 
     }
