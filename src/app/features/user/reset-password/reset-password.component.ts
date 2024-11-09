2f71b2309a97433feb7e4750f5f277fc12613e2c
kalonjis <kalonjistephane@gmail.com>
2024-11-09 15:25:55 +0100

    Refactoring : use of FeedbackBase + handling error for connected user trying to access this link

--- src/app/features/user/reset-password/reset-password.component.ts
+++ src/app/features/user/reset-password/reset-password.component.ts
@@ -4,6 +4,9 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {UserService} from '../tools/user.service';
 import {passwordStrengthValidator} from '../tools/validators/password-strength-validator';
 import {HttpErrorResponse} from '@angular/common/http';
+import {AuthService} from '../../../auth.service';
+import {FeedbackBase} from '../../../shared/feedback/tools/feedback.base';
+import {ApiResponse} from '../tools/models/api-response';
 
 @Component({
   selector: 'app-reset-password',
@@ -14,32 +17,40 @@ export class ResetPasswordComponent implements OnInit {
   newPasswordForm: FormGroup;
   errorMessage?: string = '' || undefined ;
   token: string = '';
-  showFeedback: boolean = false;
-  feedbackMessage: string = '';
-  isSuccess: boolean = true;
-  buttonText: string = '';
-  buttonAction: () => void = () => {};
+  resetFeedback: FeedbackBase = new FeedbackBase();
+
 
   constructor(
-      private _fb: FormBuilder,
-      private _router: Router,
-      private _route: ActivatedRoute,
-      private _userService: UserService,
-    ) {
-      this.newPasswordForm = this._fb.group({
-        newPassword: ['', [Validators.required, passwordStrengthValidator()]],
-        confirmPassword: ['', Validators.required]
-      }, { validators: this.passwordsMatchValidator });
+    private _fb: FormBuilder,
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _userService: UserService,
+    private _authService: AuthService,
+  ) {
+    this.newPasswordForm = this._fb.group({
+      newPassword: ['', [Validators.required, passwordStrengthValidator()]],
+      confirmPassword: ['', Validators.required]
+    }, { validators: this.passwordsMatchValidator });
   }
 
 
   ngOnInit() {
+
+    if(this._authService.isConnected()){
+      let errorMessage: string = "You are already logged in! !! Access forbidden! To change your password, go to : “settings” -> “security” -> “change my password”. "
+      this.resetFeedback.displayError(errorMessage, "Home");
+      this.resetFeedback.buttonAction = () => { this._navToHome() }
+    }
+
     this._route.queryParams.subscribe(param => {
       this.token = param['token'] || '';
+
       if (!this.token) {
-        this.errorMessage = 'Token is missing in the URL';
+        this.resetFeedback.displayError('Token is missing in the URL', 'Home')
+        this.resetFeedback.buttonAction = () => { this._navToHome() }
       }
-    })
+    });
+
   }
 
 
@@ -60,59 +71,66 @@ export class ResetPasswordComponent implements OnInit {
 
   onSubmit(): void {
     if (this.newPasswordForm.valid) {
-      const newPassword = this.newPasswordForm.value.newPassword;
-      const confirmPassword = this.newPasswordForm.value.confirmPassword;
+      const newPassword = this.newPasswordForm.controls['newPassword'].value;
+      const confirmPassword = this.newPasswordForm.controls['confirmPassword'].value;
 
 
       this._userService.resetPassword(this.token, newPassword, confirmPassword).subscribe({
-        next: () => {
-          this.feedbackMessage = "Thank you! Your password has been successfully modified. You can now use it to connect into your favorite app.";
-          this.isSuccess = true;
-          this.showFeedback = true;
-          this.buttonText = 'Back to login';
-          this.buttonAction = ()=>{
-            this._router.navigate(['user/login']);
-          }
+        next: (data: ApiResponse) => {
+          this.resetFeedback.displaySuccess(data.message, 'login');
+          this.resetFeedback.buttonAction = () => { this._navToLogin() }
 
         },
-        error: (error: HttpErrorResponse) => {
-          console.log('Contenu de error.error : ', error.error); // Debug pour l'erreur
-
-          if (error.error && typeof error.error === 'object' && error.error.error) {
-            this.feedbackMessage = error.error.error;
-          } else {
-            this.feedbackMessage = 'An unexpected error occurred. Please try again.';
-          }
-
-          this.isSuccess = false;
-          this.showFeedback = true;
-          this.buttonText = 'Request new password';
-          this.buttonAction = () => {
-            if (error.error.url) {
-              this._userService.requestNewLink(error.error.url).subscribe({
-                next: () => {
-                  this.feedbackMessage = "We've just sent you an email with instructions to reset your password. Please check your inbox.";
-                  this.isSuccess = true;
-                  this.showFeedback = true;
-                  this.buttonText = 'Back to login';
-                  this.buttonAction = () => {
-                    this._router.navigate(['user/login']);
-                  };
-                },
-                error: (requestError: HttpErrorResponse) => {
-                  console.error("Error requesting new token: ", requestError);
-                  this.feedbackMessage = 'Unable to request a new password reset token. Please try again later.';
-                  this.isSuccess = false;
-                  this.showFeedback = true;
-                  this.buttonText = 'Retry';
-                }
-              });
-            } else {
-              this._router.navigate(['/user/request-new-password']);
-            }
-          };
+        error: (error: HttpErrorResponse) : void => {
+          this._handelError(error);
         }
       });
     }
+
   }
+
+  private _handelError(error: HttpErrorResponse) : void{
+    console.log('Contenu de error.error : ', error.error);
+    if (error.error.url){
+      this.resetFeedback.displayError(error.error.error, 'Request new password');
+      this.resetFeedback.buttonAction = () => {
+        this._userService.requestNewLink(error.error.url).subscribe({
+          next: (data: ApiResponse) => {
+            this.resetFeedback.displaySuccess(data.message, 'login');
+            this.resetFeedback.buttonAction = () => { this._navToLogin() }
+          },
+          error: (error: HttpErrorResponse) => {
+            this.resetFeedback.displayError(error.error.error, 'Request new password');
+            this.resetFeedback.buttonAction = () => this._navToRequestNewPassword();
+          }
+        })
+      }
+    } else if(error.error){
+        this.resetFeedback.displayError(error.error, 'Request new password');
+        this.resetFeedback.buttonAction = () => this._navToRequestNewPassword();
+    } else {
+        const feedBackMessage: string = "An unexpected error occurred. Please try again.";
+        this.resetFeedback.displayError(feedBackMessage, 'Request new password');
+        this.resetFeedback.buttonAction = () => this._navToRequestNewPassword();
+    }
+  }
+
+  private _nav(url: string): void{
+    this._router.navigate([url]);
+  }
+
+  private _navToLogin(): void {
+    this._nav('/user/login');
+  }
+
+  private _navToHome(): void{
+    this._nav('');
+  }
+
+  private _navToRequestNewPassword(): void{
+    this._nav('/user/request-new-password');
+  }
+
 }
+
+
