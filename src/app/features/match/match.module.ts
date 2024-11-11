167a174c739d59b1ed8c3ee49b26f17eb719e097
kalonjis <kalonjistephane@gmail.com>
2024-11-11 21:24:44 +0100

    declare new "matchDetailsComponent"

--- src/app/features/match/match.module.ts
+++ src/app/features/match/match.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { MatchRoutingModule } from './match-routing.module';
 import {SharedModule} from 'primeng/api';
 import { MatchListComponent } from './match.list/match.list.component';
+import { MatchDetailsComponent } from './match.details/match.details.component';
 
 
 @NgModule({
   declarations: [
-    MatchListComponent
+    MatchListComponent,
+    MatchDetailsComponent,
   ],
   imports: [
     CommonModule,
