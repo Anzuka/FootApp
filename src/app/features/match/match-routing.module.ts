1cd2a8bb4831790c7859d97c7220219e32080f14
kalonjis <kalonjistephane@gmail.com>
2024-11-11 21:23:23 +0100

    new path for matchDetailsComponent: 'details/:id'

--- src/app/features/match/match-routing.module.ts
+++ src/app/features/match/match-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from '../home/home.component';
 import {MatchListComponent} from './match.list/match.list.component';
+import {MatchDetailsComponent} from './match.details/match.details.component';
 
 const routes: Routes = [
   { path: 'list', component: MatchListComponent },
+  { path: 'details/:id', component: MatchDetailsComponent },
 ];
 
 @NgModule({
