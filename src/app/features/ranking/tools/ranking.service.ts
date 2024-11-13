dc1f1f20014d46659e934bf4a38306b1fc502d9c
kalonjis <kalonjistephane@gmail.com>
2024-11-13 22:41:30 +0100

    new rankingService + method "getAllByTournamentIdAndTeam"

---
+++ src/app/features/ranking/tools/ranking.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {RankingModel} from './ranking.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RankingService {
+
+  private apiUrl: string = "http://localhost:8080/api/ranking";
+
+  constructor( private _http: HttpClient) { }
+
+  getAllByTournamentIdAndTeam(tournamentId: number, teamId: number): Observable<RankingModel[]> {
+    const params = new HttpParams()
+      .set('tournamentId', tournamentId.toString())
+      .set('teamId', teamId.toString());
+
+    return this._http.get<RankingModel[]>(`${this.apiUrl}/by-tournament-team`, { params });
+  }
+}
