41cb84e59cd567721754e96956a6873390da54c7
kalonjis <kalonjistephane@gmail.com>
2024-11-13 22:44:13 +0100

    import and declare ranking component

--- src/app/features/match/match.module.ts
+++ src/app/features/match/match.module.ts
@@ -5,17 +5,19 @@ import { MatchRoutingModule } from './match-routing.module';
 import {SharedModule} from 'primeng/api';
 import { MatchListComponent } from './match.list/match.list.component';
 import { MatchDetailsComponent } from './match.details/match.details.component';
+import {RankingComponent} from '../ranking/ranking.component';
 
 
 @NgModule({
   declarations: [
     MatchListComponent,
     MatchDetailsComponent,
+    RankingComponent,
   ],
   imports: [
     CommonModule,
     MatchRoutingModule,
-    SharedModule
+    SharedModule,
   ]
 })
 export class MatchModule { }
