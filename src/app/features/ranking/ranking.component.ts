1d3b35de21e92b6d1a235983e043f38cb7df91e5
kalonjis <kalonjistephane@gmail.com>
2024-11-13 22:43:43 +0100

    new ranking component

--- src/app/features/ranking/ranking.component.ts
+++ src/app/features/ranking/ranking.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+// ranking.component.ts
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { RankingModel } from './tools/ranking.model';
+import {map, Observable, of} from 'rxjs';
 
 @Component({
   selector: 'app-ranking',
   templateUrl: './ranking.component.html',
-  styleUrl: './ranking.component.scss'
+  styleUrls: ['./ranking.component.scss']
 })
-export class RankingComponent {
+export class RankingComponent implements OnInit {
+  @Input() rankings!: RankingModel[]; // Utilisation de @Input pour recevoir les données
+  @Output() rankingLoaded = new EventEmitter<boolean>();
+  sortedRankings$!: Observable<RankingModel[]>;
 
+  ngOnInit() {
+    console.log("rankings from ranking: ",this.rankings);
+    this.rankingLoaded.emit(true);// Les données de classement devraient apparaître ici
+
+    this.sortedRankings$ = of(this.rankings).pipe(
+      map(rankings => rankings.sort((a, b) => a.rankingPosition - b.rankingPosition))
+    );
+  }
 }
