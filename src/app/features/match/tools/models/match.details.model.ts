c43dbaa30295e606888787ce3f81002976251760
kalonjis <kalonjistephane@gmail.com>
2024-11-13 22:46:40 +0100

    add tournamentInfo of the match context from the back

--- src/app/features/match/tools/models/match.details.model.ts
+++ src/app/features/match/tools/models/match.details.model.ts
@@ -2,11 +2,13 @@ import {TeamModel} from './team.model';
 import {MatchStatus} from './match.status';
 import {MatchStage} from './match.stage';
 import {RefereeDTO} from './refereeDTO';
+import {TournamentSmallDetailsDTO} from './tournament.small.details-dto';
 
 export interface MatchDetailsModel {
   id: number;
   teamHome: TeamModel;
   teamAway: TeamModel;
+  tournamentSmallDetailsDTO: TournamentSmallDetailsDTO;
   refereeDTO: RefereeDTO | null;
   scoreTeamHome: number;
   scoreTeamAway: number;
