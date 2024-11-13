6710e21104394d6895561a7c6f445bc0332f1c44
kalonjis <kalonjistephane@gmail.com>
2024-11-13 12:45:09 +0100

    new ranking component: only creation, nothing implemented

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -4,12 +4,14 @@ import { FeaturesRoutingModule } from './features-routing.module';
 import { HomeComponent } from './home/home.component';
 import {UserModule} from './user/user.module';
 import {SharedModule} from '../shared/shared.module';
+import { RankingComponent } from './ranking/ranking.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    RankingComponent,
 
   ],
   imports: [
